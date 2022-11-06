fc849ea2d614e6902d46b604f5f31ad84e2eef31
mannybennett <mannybennett99@gmail.com>
2022-11-05 20:12:54 -0400

    about me page and contact

--- README.md
+++
@@ -1,2 +0,0 @@
-# ACA_web101_Emanuel_Bennett
-This is my ACA portfolio
---
+++ myPortfolio/scripts.js
@@ -0,0 +1,15 @@
+const placeForResults = document.getElementById("results")
+
+new URLSearchParams(window.location.search).forEach((value, name) => {
+  placeForResults.append(`${name} : ${value}`)
+  placeForResults.append(document.createElement("br"))
+})
+
+
+
+
+
+
+
+
+
